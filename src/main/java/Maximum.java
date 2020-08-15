d2c6cf5a1dcba8f30bbe6491b939ad6b83f2d98e
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-16 00:58:15 +0530

    Added: Method & Test cases to find maximum integer number.

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,2 +1,16 @@
 public class Maximum {
-}
+    //Method to check maximum integer number between three integers
+     public Integer testMaximumInteger (Integer x, Integer y, Integer z){
+            Integer maximumNumber = z;
+
+            if (y.compareTo(maximumNumber) > 0) {
+                maximumNumber = y;
+
+            } else if (x.compareTo(maximumNumber) > 0) {
+                maximumNumber = x;
+            } else
+                maximumNumber = z;
+
+            return maximumNumber;
+        }
+    }
--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -1,2 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaximumTest {
+    //Test cases for Maximum Integer
+    @Test
+    public void givenMaxValue_atPos1_shouldReturn_maximumNumber() {
+        Maximum maximum =new Maximum();
+        int maximumNumber=maximum.testMaximumInteger(6,4,5);
+        Assert.assertEquals(6,maximumNumber);
+    }
+
+    @Test
+    public void givenMaxValue_atPos2_shouldReturn_maximumNumber() {
+        Maximum maximum=new Maximum();
+        int maximumNumber=maximum.testMaximumInteger(7,8,3);
+        Assert.assertEquals(8,maximumNumber);
+    }
+
+    @Test
+    public void givenMaxValue_atPos3_shouldReturn_maximumNumber() {
+        Maximum maximum=new Maximum();
+        int maximumNumber=maximum.testMaximumInteger(10,12,13);
+        Assert.assertEquals(13,maximumNumber);
+    }
 }
