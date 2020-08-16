a2e8f36351c99ca2cedfc9c873dd68fd7d793e5a
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-16 17:37:53 +0530

    Added: Method & test cases for find maximum float number.

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,16 +1,33 @@
 public class Maximum {
-    //Method to check maximum integer number between three integers
+    //Method to check maximum integer number between three integers.
      public Integer testMaximumInteger (Integer x, Integer y, Integer z){
             Integer maximumNumber = z;
 
             if (y.compareTo(maximumNumber) > 0) {
                 maximumNumber = y;
-
-            } else if (x.compareTo(maximumNumber) > 0) {
+            }
+            else if (x.compareTo(maximumNumber) > 0) {
                 maximumNumber = x;
-            } else
+            }
+            else
                 maximumNumber = z;
 
             return maximumNumber;
         }
-    }
+
+     //Method to check maximum float number between three float numbers.
+     public Float testMaximumFloat (Float number1, Float number2, Float number3){
+         Float maximumFloat = number3;
+
+         if (number2.compareTo(maximumFloat) > 0) {
+             maximumFloat = number2;
+         }
+         else if (number1.compareTo(maximumFloat) > 0) {
+             maximumFloat = number1;
+         }
+         else
+             maximumFloat = number3;
+
+         return maximumFloat;
+     }
+}
--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -4,23 +4,45 @@ import org.junit.Test;
 public class MaximumTest {
     //Test cases for Maximum Integer
     @Test
-    public void givenMaxValue_atPos1_shouldReturn_maximumNumber() {
+    public void givenMaxValue_atPosition1_shouldReturn_maximumNumber() {
         Maximum maximum =new Maximum();
         int maximumNumber=maximum.testMaximumInteger(6,4,5);
-        Assert.assertEquals(6,maximumNumber);
+        Assert.assertEquals(6, maximumNumber);
     }
 
     @Test
-    public void givenMaxValue_atPos2_shouldReturn_maximumNumber() {
+    public void givenMaxValue_atPosition2_shouldReturn_maximumNumber() {
         Maximum maximum=new Maximum();
         int maximumNumber=maximum.testMaximumInteger(7,8,3);
-        Assert.assertEquals(8,maximumNumber);
+        Assert.assertEquals(8, maximumNumber);
     }
 
     @Test
-    public void givenMaxValue_atPos3_shouldReturn_maximumNumber() {
+    public void givenMaxValue_atPosition3_shouldReturn_maximumNumber() {
         Maximum maximum=new Maximum();
         int maximumNumber=maximum.testMaximumInteger(10,12,13);
-        Assert.assertEquals(13,maximumNumber);
+        Assert.assertEquals(13, maximumNumber);
+    }
+
+    //Test cases for maximum float
+    @Test
+    public void givenMaxValue_atPosition1_shouldReturn_maximumFloat() {
+        Maximum maximum =new Maximum();
+        Comparable maximumFloat=maximum.testMaximumFloat(6.1f, 4.2f, 5.3f);
+        Assert.assertEquals(6.1f, maximumFloat);
+    }
+
+    @Test
+    public void givenMaxValue_atPosition2_shouldReturn_maximumFloat() {
+        Maximum maximum=new Maximum();
+        Comparable maximumFloat=maximum.testMaximumFloat(7.1f,8.2f,3.3f);
+        Assert.assertEquals(8.2f, maximumFloat);
+    }
+
+    @Test
+    public void givenMaxValue_atPosition3_shouldReturn_maximumFloat() {
+        Maximum maximum=new Maximum();
+        Comparable maximumFloat=maximum.testMaximumFloat(10.1f,12.2f,13.4f);
+        Assert.assertEquals(13.4f, maximumFloat);
     }
 }
